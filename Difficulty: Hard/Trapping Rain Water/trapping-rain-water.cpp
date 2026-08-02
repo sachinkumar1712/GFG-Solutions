77a6333700b0e28262b6477e7e99e37b460c7695
Sachin Kumar <sachinbaghel1712@gmail.com>
2026-08-02 22:33:49 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Trapping Rain Water/trapping-rain-water.cpp
@@ -0,0 +1,24 @@
+class Solution {
+  public:
+    int maxWater(vector<int> &arr) {
+        // code here
+        int n = arr.size();
+        vector<int>lmax(n,0);
+        vector<int>rmax(n,0);
+        lmax[0]=arr[0];
+        rmax[n-1]=arr[n-1];
+        for(int i=1;i<n;i++){
+            lmax[i]=max(lmax[i-1],arr[i]);
+            
+            
+        }
+        for(int i=n-2;i>=0;i--){
+            rmax[i]=max(rmax[i+1],arr[i]);
+        }
+        int ans = 0;
+        for(int i=0;i<n;i++){
+            ans+=min(lmax[i],rmax[i])-arr[i];
+        }
+        return ans;
+    }
+};
